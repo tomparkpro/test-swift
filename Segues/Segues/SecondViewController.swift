170961b7b43fb5b9298fa93616839f6052c8b43e
tomparkpro <tom.hyunung.park@gmail.com>
2018-06-11 10:50:40 +0900

    add Delegate protocol

--- Segues/Segues/SecondViewController.swift
+++ Segues/Segues/SecondViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
+protocol CanReceive {
+    func dataReceived(data: String)
+}
 class SecondViewController: UIViewController {
-
+    
+    var delegate : CanReceive?
     var textPassedOver : String?
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,20 +25,10 @@ class SecondViewController: UIViewController {
         label.text = textPassedOver
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sendDataBack(_ sender: UIButton) {
+        delegate?.dataReceived(data: textField.text!)
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Segues/Segues/ViewController.swift
+++ Segues/Segues/ViewController.swift
@@ -8,21 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CanReceive {
 
+
+    @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
         performSegue(withIdentifier: "goToSecondScreen", sender: self)
+    }
+    
+    @IBAction func changeToBlue(_ sender: UIButton) {
+        view.backgroundColor = UIColor.blue
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -30,9 +31,13 @@ class ViewController: UIViewController {
             let destinationVC = segue.destination as! SecondViewController
             
             destinationVC.textPassedOver = textField.text!
+            destinationVC.delegate = self
         }
         
     }
     
+    func dataReceived(data: String) {
+        label.text = data
+    }
 }
 
